a8da0a30d4af6c6b84f610d68e443ed2261a71bf
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-09-12 15:26:22 -0700

    And really make sure we write them in UTF-8

--- lib/DataWriter.groovy
+++ lib/DataWriter.groovy
@@ -13,11 +13,11 @@ public class DataWriter {
         // write unsigned data to *.json because of JENKINS-15105
         File d = new File("target")
         d.mkdirs()
-        new File(d,"${key}.json").write("downloadService.post('${key}',${envelope.toString(2)})");
+        new File(d,"${key}.json").write("downloadService.post('${key}',${envelope.toString(2)})","UTF-8");
 
         // then signed data to *.json.html
         if (System.getenv("JENKINS_SIGNER")!=null)
             new Signer().configureFromEnvironment().sign(envelope);
-        new File(d,"${key}.json.html").write("\uFEFF<!DOCTYPE html><html><head><meta http-equiv='Content-Type' content='text/html;charset=UTF-8' /></head><body><script>window.onload = function () { window.parent.postMessage(JSON.stringify(\n${envelope.toString(2)}\n),'*'); };</script></body></html>");
+        new File(d,"${key}.json.html").write("\uFEFF<!DOCTYPE html><html><head><meta http-equiv='Content-Type' content='text/html;charset=UTF-8' /></head><body><script>window.onload = function () { window.parent.postMessage(JSON.stringify(\n${envelope.toString(2)}\n),'*'); };</script></body></html>","UTF-8");
     }
 }
