7d7171fa81aa7634a334b2e641a8b40440903d2b
wolfg1969 <wolfg1969@gmail.com>
2012-12-24 13:44:55 +0800

    download sbt.zip instead of sbt-launch.jar

--- sbt.groovy
+++ sbt.groovy
@@ -6,7 +6,7 @@ import com.gargoylesoftware.htmlunit.WebClient
 import net.sf.json.*
 
 def wc = new WebClient()
-def baseUrl = 'http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/'
+def baseUrl = 'http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/'
 HtmlPage p = wc.getPage(baseUrl);
 
 def json = [];
@@ -17,7 +17,7 @@ p.selectNodes("//a[@href]").reverse().collect { HtmlAnchor e ->
     def m = (url =~ /([0-9]{1,}\.[0-9]{2}\.[0-9]{1}(-.*)?)\/$/)
     if (m) {
         println m[0][1]
-        json << ["id":m[0][1], "name": "sbt-launch.jar ${m[0][1]}".toString(), "url":url + "sbt-launch.jar"];
+        json << ["id":m[0][1], "name": "sbt ${m[0][1]}".toString(), "url":url + "sbt.zip"];
     }
 }
 
