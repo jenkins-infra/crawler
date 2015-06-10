ad76c4ddad4786033b7a81bef7f15f54cf8e19c0
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-06-10 10:55:46 -0700

    Need to use a newer groovy

--- ci.sh
+++ ci.sh
@@ -2,6 +2,9 @@
 # Script used on Jenkins to build & publish data
 export JENKINS_SIGNER="-key \"$SECRET/update-center.key\" -certificate \"$SECRET/update-center.cert\" -root-certificate \"$SECRET/jenkins-update-center-root-ca.crt\""
 
+# need to pick up newer groovy
+export PATH=/usr/local/groovy/bin:$PATH
+
 mvn -e clean install
 
 for f in *.groovy
