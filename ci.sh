22b931767bf9e6485f73c38f26f1fb21732a52cd
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-06-10 10:45:43 -0700

    [INFRA-75] Script to be run from ci.jenkins-ci.org

---
+++ ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+# Script used on Jenkins to build & publish data
+export JENKINS_SIGNER="-key \"$SECRET/update-center.key\" -certificate \"$SECRET/update-center.cert\" -root-certificate \"$SECRET/jenkins-update-center-root-ca.crt\""
+
+mvn -e clean install
+
+for f in *.groovy
+do
+  ./$f || true
+done
+
+mkdir updates || true
+cp target/*.json target/*.html updates
+cd updates
+
+# pushing the tool update data to updates.jenkins-ci.org
+cd ..
+rsync -avz --exclude=.svn updates/ www-data@updates.jenkins-ci.org:/var/www/updates.jenkins-ci.org/updates/
+rsync -avz --exclude=.svn updates/ www-data@updates.jenkins-ci.org:/var/www/updates2.jenkins-ci.org/updates/
+
