686e8ffceaa174d7653614bff125fe5b47f60268
kiy0taka <kiy0taka333@gmail.com>
2015-06-19 15:11:51 +0900

    use GVM API for grails

--- grails.groovy
+++ grails.groovy
@@ -1,24 +1,11 @@
 #!./lib/runner.groovy
 // Generates server-side metadata for MongoDB auto-installation
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1' )
-import groovyx.net.http.*
-import com.gargoylesoftware.htmlunit.html.*;
 import net.sf.json.*
-import com.gargoylesoftware.htmlunit.WebClient
 
-def wc = new WebClient()
-def json = [];
-
-def http = new HttpURLClient(url:'http://www.grails.org', followRedirects: false)
-HtmlPage p = wc.getPage('http://www.grails.org/download')
-def xpath = "//section[@id='downloadArchives']//table[@class='table']//tr/td[4]/a"
-p.selectNodes(xpath).collect { HtmlAnchor a ->
-    def url = a.getHrefAttribute()
-    println url
-    def m = url =~/\/grails(?:-bin)?-(.*)\.zip/
-    if (m) {
-        json << [id:m[0][1], name:"Grails ${m[0][1]}".toString(), url:url]
-    }
+def json = []
+def versions = new URL('http://api.gvmtool.net/candidates/grails').text.split(',').reverse()
+versions.each {
+    json << [id:it, name:"Grails $it".toString(), url:"https://github.com/grails/grails-core/releases/download/v${it}/grails-${it}.zip".toString()]
 }
 
 lib.DataWriter.write("com.g2one.hudson.grails.GrailsInstaller",JSONObject.fromObject([list:json]));
