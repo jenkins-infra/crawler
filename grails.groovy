ca9165b40bfa7f22e6ee75e02101e1315ce78e27
kiy0taka <kiy0taka333@gmail.com>
2015-12-31 10:10:21 +0900

    gvmtool -> sdkman

--- grails.groovy
+++ grails.groovy
@@ -3,7 +3,7 @@
 import net.sf.json.*
 
 def json = []
-def versions = new URL('http://api.gvmtool.net/candidates/grails').text.split(',').reverse()
+def versions = new URL('http://api.sdkman.io/candidates/grails').text.split(',').reverse()
 versions.each {
     json << [id:it, name:"Grails $it".toString(), url:"https://github.com/grails/grails-core/releases/download/v${it}/grails-${it}.zip".toString()]
 }
