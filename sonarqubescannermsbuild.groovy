22d4543bd882b9a27d3e08eec01e0e77e8be3cc5
Julien HENRY <henryju@yahoo.fr>
2017-03-29 16:58:27 +0200

    Update URLs for SonarQube Scanner for MSBuild

--- sonarqubescannermsbuild.groovy
+++ sonarqubescannermsbuild.groovy
@@ -2,7 +2,7 @@
 // Generates server-side metadata for MSBuild SonarQube Runner
 import net.sf.json.*
 
-def url = "https://api.github.com/repos/SonarSource-VisualStudio/sonar-msbuild-runner/releases".toURL()
+def url = "https://api.github.com/repos/SonarSource/sonar-scanner-msbuild/releases".toURL()
 def releases = JSONArray.fromObject(url.text)
 
 def json = []
@@ -11,7 +11,7 @@ for (JSONObject release : releases) {
   if (!release.get("draft") && !release.get("prerelease") && !release.get("tag_name").toLowerCase().contains("vsts")) {
     json << ["id": release.get("tag_name"),
              "name": "SonarQube Scanner for MSBuild ${release.get("tag_name")}".toString(), 
-             "url": "https://github.com/SonarSource-VisualStudio/sonar-msbuild-runner/releases/download/${release.get("tag_name")}/MSBuild.SonarQube.Runner-${release.get("tag_name")}.zip".toString()];
+             "url": "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${release.get("tag_name")}/MSBuild.SonarQube.Runner-${release.get("tag_name")}.zip".toString()];
   }
 }
 
