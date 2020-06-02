0574c391d65c066bcac978fa7c0ffeaef6e5377d
Tim Van Holder <tim.vanholder@gmail.com>
2020-06-03 00:56:30 +0200

    Adjust SDK package properties
    This renames platform to rid, and adds a new platform property with a
    more readable version of the RID.
    
    Code cleanup: single quoted strings, no spurious semicolons.

--- dotnetSdk.groovy
+++ dotnetSdk.groovy
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils
 
 static WebClient createWebClient() {
     final WebClient wc = new WebClient()
-    wc.setCssErrorHandler(new SilentCssErrorHandler());
+    wc.setCssErrorHandler(new SilentCssErrorHandler())
     wc.options.javaScriptEnabled = false
     wc.options.throwExceptionOnFailingStatusCode = false
     wc.options.throwExceptionOnScriptError = false
@@ -27,8 +27,8 @@ void createDownloadable(String className, JSONObject object) {
 static JSONObject fetchJson(String url) {
     //System.out.printf('fetching JSON from %s%n', url)
     final WebClient wc = createWebClient()
-    final Page p = wc.getPage(url);
-    return JSONObject.fromObject(p.webResponse.contentAsString);
+    final Page p = wc.getPage(url)
+    return JSONObject.fromObject(p.webResponse.contentAsString)
 }
 
 
@@ -52,10 +52,10 @@ createFrameworkMonikers()
 
 private void createRidCatalog() {
     final JSONObject ridCatalog = fetchJson('https://raw.githubusercontent.com/dotnet/runtime/master/src/libraries/pkg/Microsoft.NETCore.Platforms/runtime.json')
-    final String[] rids = ridCatalog.getJSONObject("runtimes").keySet().toArray()
+    final String[] rids = ridCatalog.getJSONObject('runtimes').keySet().toArray()
     // TODO: Maybe sort this list so that names with fewer parts sort before those with more.
-    // TODO: Specifically, sort "tizen-4.0.0" and "tizen-5.0.0" before "tizen-4.0.0-x64" and "tizen-5.0.0-x64"
-    final JSONObject data = JSONObject.fromObject([ "ridCatalog": rids ])
+    // TODO: Specifically, sort 'tizen-4.0.0' and 'tizen-5.0.0' before 'tizen-4.0.0-x64' and 'tizen-5.0.0-x64'
+    final JSONObject data = JSONObject.fromObject([ 'ridCatalog': rids ])
     createDownloadable('Runtime', data)
 }
 
@@ -67,7 +67,7 @@ createRidCatalog()
 
 private String getSdkInfo(JSONObject s) {
     final StringBuilder info = new StringBuilder()
-    def value = s.get('vs-support');
+    def value = s.get('vs-support')
     if (value instanceof String && !value.isEmpty())
         info.append(info)
     else {
@@ -119,12 +119,81 @@ private def getSdk(sdks, JSONObject s) {
     def packages = []
     def urls = []
     for (JSONObject p : s.getJSONArray('files')) {
-        final String fileName = p.getString('name');
+        final String fileName = p.getString('name')
         if (fileName == null || (!fileName.endsWith('.zip') && !fileName.endsWith('.tar.gz')))
             continue
         def pkg = [:]
-        pkg['platform'] = p.getString('rid')
-        // FIXME: Maybe store the hash too?
+        final String rid = p.getString('rid')
+        pkg['rid'] = rid
+        if (rid != null) {
+            String[] parts = rid.split(/[-]/, 2)
+            String osAndVersion = parts[0]
+            String arch = parts.length > 1 ? parts[1] : null
+            parts = osAndVersion.split(/[.]/)
+            String os = parts[0]
+            String version = parts.length > 1 ? parts[1] : null
+            if (arch != null) {
+                switch (arch) {
+                    case 'x86':
+                    case 'x64':
+                        // ok, fine as-is
+                        break;
+                    case 'musl-x64':
+                        // move the musl to os, so we can map that to Alpine Linux
+                        os += '-musl'
+                        arch = 'x64'
+                        break;
+                    case 'arm':
+                        arch = 'ARM32'
+                        break;
+                    case 'arm64':
+                        arch = 'ARM64'
+                        break;
+                    default:
+                        //System.err.printf('NO MAPPING DEFINED FOR ARCH PART (%s) OF RID "%s"%n', arch, rid)
+                        arch = '???'
+                }
+            }
+            switch (os) {
+                case 'centos':
+                    os = 'CentOS'
+                    break;
+                case 'debian':
+                    os = 'Debian'
+                    break;
+                case 'fedora':
+                    os = 'Fedora'
+                    break;
+                case 'linux':
+                    os = 'Linux'
+                    break;
+                case 'linux-musl':
+                    os = 'Alpine Linux'
+                    break;
+                case 'opensuse':
+                    os = 'OpenSUSE'
+                    break;
+                case 'osx':
+                    os = 'macOS'
+                    break;
+                case 'rhel':
+                    os = 'RHEL'
+                    break;
+                case 'ubuntu':
+                    os = 'Ubuntu'
+                    break;
+                case 'win':
+                    os = 'Windows'
+                    break;
+                default:
+                    //System.err.printf('NO MAPPING DEFINED FOR OS PART (%s) OF RID "%s"%n', os, rid)
+                    os = '???'
+            }
+            version = (version != null) ? (' ' + version) : ''
+            arch = (arch != null) ? ' - ' + arch : ''
+            pkg['platform'] = os + version + arch
+        }
+        // FIXME: Maybe store the hash too? The installer could potentially verify it after the download.
         final String url = p.getString('url')
         pkg['url'] = url
         urls += url
@@ -132,7 +201,7 @@ private def getSdk(sdks, JSONObject s) {
     }
     if (urls.size() > 0) {
         String urlPrefix = StringUtils.getCommonPrefix(urls as String[])
-        if (!urlPrefix.endsWith("/"))
+        if (!urlPrefix.endsWith('/'))
             urlPrefix = urlPrefix.substring(0, urlPrefix.lastIndexOf('/') + 1)
         if (urlPrefix.size() > 15) { // shorter is not worth emitting the property for
             for (def pkg : packages)
@@ -147,52 +216,50 @@ private def getSdk(sdks, JSONObject s) {
 
 private void createSdkDownloads() {
     def versions = [];
-    def sdks = [:];
-    {
-        JSONObject releaseIndex = fetchJson('https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json')
-        for (JSONObject v : releaseIndex.getJSONArray("releases-index")) {
-            def version = [:]
-            version['name'] = v.getString('product') + ' ' + v.getString('channel-version')
-            version['status'] = v.getString('support-phase').toUpperCase()
-            version['endOfSupport'] = v.get('eol-date')
-            def releases = []
-            final JSONObject channel = fetchJson(v.getString('releases.json'))
-            for (JSONObject r : channel.getJSONArray("releases")) {
-                def release = [:]
-                release['name'] = r.getString('release-version')
-                release['released'] = r.getString('release-date')
-                if (r.getBoolean('security'))
-                    release['securityFixes'] = true
-                // Assumption based on Semantic Versioning
-                if (r.getString('release-version').contains('-'))
-                    release['preview'] = true
-                // FIXME: Should we include the release notes too? Might be nice to be able to show a link to those in
-                // FIXME: the tool installer UX.
-                release['sdks'] = []
-                // Older releases have only 'sdk'. Some have sdks: null. But when sdks is set, it always includes sdk.
-                def releaseSdks = r.get('sdks')
-                if (releaseSdks instanceof JSONObject && releaseSdks.isNullObject())
-                    releaseSdks = null
-                if (releaseSdks == null) {
-                    def s = r.get('sdk')
-                    if (s instanceof JSONObject && s.isNullObject())
-                        s = null
-                    if (s != null) {
-                        def sdk = getSdk(sdks, s)
-                        release['sdks'] += sdk['name']
-                    }
+    def sdks = [:]
+    JSONObject releaseIndex = fetchJson('https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json')
+    for (JSONObject v : releaseIndex.getJSONArray('releases-index')) {
+        def version = [:]
+        version['name'] = v.getString('product') + ' ' + v.getString('channel-version')
+        version['status'] = v.getString('support-phase').toUpperCase()
+        version['endOfSupport'] = v.get('eol-date')
+        def releases = []
+        final JSONObject channel = fetchJson(v.getString('releases.json'))
+        for (JSONObject r : channel.getJSONArray('releases')) {
+            def release = [:]
+            release['name'] = r.getString('release-version')
+            release['released'] = r.getString('release-date')
+            if (r.getBoolean('security'))
+                release['securityFixes'] = true
+            // Assumption based on Semantic Versioning
+            if (r.getString('release-version').contains('-'))
+                release['preview'] = true
+            // FIXME: Should we include the release notes too? Might be nice to be able to show a link to those in
+            // FIXME: the tool installer UX.
+            release['sdks'] = []
+            // Older releases have only 'sdk'. Some have sdks: null. But when sdks is set, it always includes sdk.
+            def releaseSdks = r.get('sdks')
+            if (releaseSdks instanceof JSONObject && releaseSdks.isNullObject())
+                releaseSdks = null
+            if (releaseSdks == null) {
+                def s = r.get('sdk')
+                if (s instanceof JSONObject && s.isNullObject())
+                    s = null
+                if (s != null) {
+                    def sdk = getSdk(sdks, s)
+                    release['sdks'] += sdk['name']
                 }
-                else {
-                    for (JSONObject s : releaseSdks) {
-                        def sdk = getSdk(sdks, s)
-                        release['sdks'] += sdk['name']
-                    }
-                }
-                releases += release
             }
-            version['releases'] = releases
-            versions += version
+            else {
+                for (JSONObject s : releaseSdks) {
+                    def sdk = getSdk(sdks, s)
+                    release['sdks'] += sdk['name']
+                }
+            }
+            releases += release
         }
+        version['releases'] = releases
+        versions += version
     }
     createDownloadable('Download', JSONObject.fromObject([ 'versions' : versions, 'sdks': sdks.values()]))
 }
