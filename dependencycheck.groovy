c10ac4bcb4d8e938b4616c34e37132e18fb0e36c
James Howe <675056+OrangeDog@users.noreply.github.com>
2020-10-02 14:32:57 +0100

    Dependency check downloads migrating to github
    I'm not 100% on how this system works, but this should fix things.
    
    https://github.com/jeremylong/DependencyCheck/releases/tag/v6.0.2
    https://issues.jenkins-ci.org/browse/JENKINS-63806

--- dependencycheck.groovy
+++ dependencycheck.groovy
@@ -1,46 +1,59 @@
 #!./lib/runner.groovy
 // Generates server-side metadata for dependencycheck-launch auto-installation
-import com.gargoylesoftware.htmlunit.html.*;
-import com.gargoylesoftware.htmlunit.WebClient
-import com.gargoylesoftware.htmlunit.xml.XmlPage
+
 import hudson.util.VersionNumber
 import net.sf.json.*
 
 def listFromBintray() {
     def url = "https://bintray.com/api/v1/packages/jeremy-long/owasp/dependency-check".toURL()
-	def bintray = JSONObject.fromObject(url.text)
+    def bintray = JSONObject.fromObject(url.text)
 
-	bintray["versions"].collect {
+    bintray["versions"].collect {
         version -> ["id": version,
                     "name": "dependency-check ${version}".toString(),
                     "url": "https://dl.bintray.com/jeremy-long/owasp/dependency-check-${version}-release.zip".toString()];
     }
 }
 
+def listFromGithub() {
+    def url = "https://api.github.com/repos/jeremylong/dependencycheck/releases".toURL()
+    def releases = JSONArray.fromObject(url.text)
+
+    releases.collect {
+        release ->
+            def version = release["name"].substring(1)
+            ["id": version,
+             "name": "dependency-check ${version}".toString(),
+             "url": "https://github.com/jeremylong/DependencyCheck/releases/download/v${version}/dependency-check-${version}-release.zip".toString()]
+    }
+}
+
 
 def listAll() {
-    List versions = new ArrayList()
-    versions.addAll(listFromBintray())
+    Map versions = new HashMap()
+    listFromBintray().each {
+        version -> versions.put(version["id"], version) }
+    listFromGithub().each {version -> versions.put(version["id"], version)}
 
-    return versions
+    return versions.values()
             .findAll { it != null }
             .sort { o1,o2 ->
-        try {
-            def v1 = new VersionNumber(o1.id)
-            try {
-                new VersionNumber(o2.id).compareTo(v1)
-            } catch (IllegalArgumentException _2) {
-                -1
+                try {
+                    def v1 = new VersionNumber(o1.id)
+                    try {
+                        new VersionNumber(o2.id).compareTo(v1)
+                    } catch (IllegalArgumentException _2) {
+                        -1
+                    }
+                } catch (IllegalArgumentException _1) {
+                    try {
+                        new VersionNumber(o2.id)
+                        1
+                    } catch (IllegalArgumentException _2) {
+                        o2.id.compareTo(o1.id)
+                    }
+                }
             }
-        } catch (IllegalArgumentException _1) {
-            try {
-                new VersionNumber(o2.id)
-                1
-            } catch (IllegalArgumentException _2) {
-                o2.id.compareTo(o1.id)
-            }
-        }
-    }
 }
 
 lib.DataWriter.write("org.jenkinsci.plugins.DependencyCheck.tools.DependencyCheckInstaller",JSONObject.fromObject([list:listAll()]));
