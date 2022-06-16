f8d60c574ebabbf1d422cb71f83994afc1c6f297
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-06-16 15:32:03 +0200

    fix(groovy.groovy): set a user agent for the hlmtunit.WebClient to get the unredirected groovy versions page (#117)

--- groovy.groovy
+++ groovy.groovy
@@ -4,8 +4,22 @@ import com.gargoylesoftware.htmlunit.html.*;
 
 import net.sf.json.*
 import com.gargoylesoftware.htmlunit.WebClient
+import com.gargoylesoftware.htmlunit.BrowserVersion
 
-def wc = new WebClient()
+final String applicationName = "crawler";
+final String applicationVersion = "1.0";
+final String userAgent = "jenkins-project-crawler";
+
+final BrowserVersion browser =
+        new BrowserVersion.BrowserVersionBuilder(BrowserVersion.BEST_SUPPORTED)
+            .setApplicationName(applicationName)
+            .setApplicationVersion(applicationVersion)
+            .setUserAgent(userAgent)
+            .build();
+
+// Set a custom user agent so the server won't redirect to https://groovy.jfrog.io/ui/native/dist-release-local/groovy-zips/
+// which contains a script with the unsuppported JS function `fetch`
+def wc = new WebClient(browser);
 def baseUrl = 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/'
 HtmlPage p = wc.getPage(baseUrl);
 
