de34286731c47b21dec762e76cda2078711e28b8
Hervé Le Meur <hlemeur@cloudbees.com>
2022-06-16 15:06:08 +0200

    Apply review suggestions and add comment

--- groovy.groovy
+++ groovy.groovy
@@ -6,9 +6,9 @@ import net.sf.json.*
 import com.gargoylesoftware.htmlunit.WebClient
 import com.gargoylesoftware.htmlunit.BrowserVersion
 
-final String applicationName = "APPNAME";
-final String applicationVersion = "APPVERSION";
-final String userAgent = "USERAGENT";
+final String applicationName = "crawler";
+final String applicationVersion = "1.0";
+final String userAgent = "jenkins-project-crawler";
 
 final BrowserVersion browser =
         new BrowserVersion.BrowserVersionBuilder(BrowserVersion.BEST_SUPPORTED)
@@ -17,6 +17,8 @@ final BrowserVersion browser =
             .setUserAgent(userAgent)
             .build();
 
+// Set a custom user agent so the server won't redirect to https://groovy.jfrog.io/ui/native/dist-release-local/groovy-zips/
+// which contains a script with the unsuppported JS function `fetch`
 def wc = new WebClient(browser);
 def baseUrl = 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/'
 HtmlPage p = wc.getPage(baseUrl);
