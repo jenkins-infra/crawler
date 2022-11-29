b6ad84a0c5b6dcb64ba8fb922fb71719e0103b5b
Damien Duportal <damien.duportal@gmail.com>
2022-11-29 09:23:03 +0100

    Apply suggestions from code review
    Co-authored-by: Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>

--- lib/signer.groovy
+++ lib/signer.groovy
@@ -12,21 +12,21 @@ dir.eachFileRecurse (FileType.FILES) { file ->
 }
 
 list.each {
-  String file  = it.path
+  String file = it.path
   println "== Checking file: ${file}"
 
-  // Load content of JSON file to be signed
+  // Load JSON file content to be signed
   File inputfile = new File(file)
   String utf8Content = inputfile.getText("UTF-8")
-  if(utf8Content.length()<1) {
+  if(utf8Content.length() < 1) {
     println "ERROR: the file ${file} is empty. Exiting."
     System.exit(1)
   }
 
   // There is JSONp callback wrapping the envelope. Separator is the first comma (',').
   int commaIndex = utf8Content.indexOf(',')
-  // Do not forget to remove the closing parenthses char at the end
-  String jsonBody = utf8Content.substring(commaIndex + 1, utf8Content.length()-1)
+  // Do not forget to remove the closing parenthesis char at the end
+  String jsonBody = utf8Content.substring(commaIndex + 1, utf8Content.length() - 1)
   if(!jsonBody.contains(':') || jsonBody.length() < 100 ) {
     println "ERROR: the JSON body in the ${file} is empty or with harmful content. Exiting."
     System.exit(1)
@@ -34,7 +34,7 @@ list.each {
 
   JSONObject jsonContent = JSONObject.fromObject(jsonBody)
 
-  // Sign the content (it adds JSON fields at thend)
+  // Sign the content (it adds JSON fields at the end)
   if (System.getenv("JENKINS_SIGNER")!=null) {
     println 'The environment variable "JENKINS_SIGNER" is defined: let\'s sign'
       new Signer().configureFromEnvironment().sign(jsonContent)
