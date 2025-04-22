1bc8616b99943b2b6de532abac68165e75cdab42
Tim Jacomb <timjacomb1@gmail.com>
2025-04-22 22:25:24 +0100

    Add all supported versions

--- chromedriver.groovy
+++ chromedriver.groovy
@@ -3,19 +3,22 @@
 import net.sf.json.JSONObject
 import groovy.json.JsonSlurper
 
-def url = 'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json'
+def url = 'https://googlechromelabs.github.io/chrome-for-testing/latest-versions-per-milestone-with-downloads.json'
 def jsonString = new URL(url).text
 def jsonData = new JsonSlurper().parseText(jsonString)
 
 def jsonList = []
 
 // Extract information only for the "Stable" channel
-def stableData = jsonData.channels.Stable
-def chromeDriverData = stableData.downloads.chromedriver
-chromeDriverData.each { entry ->
-    def id = "${entry.platform}_${stableData.version}".toString()
-    def entry_url = entry.url
-    jsonList << ["id": id, "url": entry_url]
+jsonData.milestones.each { milestone ->
+    def milestoneData = milestone.value
+
+    def chromeDriverData = milestoneData.downloads.chromedriver
+    chromeDriverData.each { entry ->
+        def id = "${entry.platform}_${milestoneData.version}".toString()
+        def entry_url = entry.url
+        jsonList << ["id": id, "url": entry_url]
+    }
 }
 
 lib.DataWriter.write("org.jenkins-ci.plugins.chromedriver.ChromeDriver",JSONObject.fromObject([list:jsonList]))
