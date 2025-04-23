e836c6b292185c6144727338f0ab17c1209cf236
Sandeep Nakka <78208500+nakka-sandeep@users.noreply.github.com>
2025-04-23 19:04:08 +0530

    Provide latest Chrome Driver releases to tools (#140)
    Co-authored-by: Mark Waite <mark.earl.waite@gmail.com>
    Co-authored-by: Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
    Co-authored-by: Tim Jacomb <timjacomb1@gmail.com>

--- chromedriver.groovy
+++ chromedriver.groovy
@@ -1,55 +1,23 @@
 #!./lib/runner.groovy
 // Generates server-side metadata for chromedriver auto-installation
-import net.sf.json.*
-import groovy.util.XmlSlurper
+import net.sf.json.JSONObject
+import groovy.json.JsonSlurper
 
-def baseUrl = 'https://chromedriver.storage.googleapis.com/'
-def json = []
-def map = [:].withDefault {[:].withDefault {[:]}}
+def url = 'https://googlechromelabs.github.io/chrome-for-testing/latest-versions-per-milestone-with-downloads.json'
+def jsonString = new URL(url).text
+def jsonData = new JsonSlurper().parseText(jsonString)
 
-def xml = new XmlSlurper().parseText(new URL(baseUrl).text)
-xml.Contents.each {
-    def key = it.Key.text()
-    def m = key =~ /(.*)\/chromedriver_(.*?)(\d+)\.zip/
-    if (m) {
-        def (_, version, os, arch) = m[0]
-        map[os][arch][version] = key
-        json << ["id":"${os}${arch}_${version}".toString(), "url":baseUrl + key];
+def jsonList = []
+
+jsonData.milestones.each { milestone ->
+    def milestoneData = milestone.value
+
+    def chromeDriverData = milestoneData.downloads.chromedriver
+    chromeDriverData.each { entry ->
+        def id = "${entry.platform}_${milestoneData.version}".toString()
+        def entry_url = entry.url
+        jsonList << ["id": id, "url": entry_url]
     }
 }
 
-map.each { os, arch ->
-    if (arch.size() == 1) {
-        def latest = arch.find().value.max { a, b ->
-            List verA = a.key.tokenize('.')
-            List verB = b.key.tokenize('.')
-            def commonIndices = Math.min(verA.size(), verB.size())
-            for (int i = 0; i < commonIndices; ++i) {
-                def numA = verA[i].toInteger()
-                def numB = verB[i].toInteger()
-                if (numA != numB) {
-                    return numA <=> numB
-                }
-            }
-            verA.size() <=> verB.size() }
-        json << ["id":os, "url":baseUrl + latest.value];
-    } else {
-        arch.each { k, v ->
-            def latest = v.max { a, b ->
-                List verA = a.key.tokenize('.')
-                List verB = b.key.tokenize('.')
-                def commonIndices = Math.min(verA.size(), verB.size())
-                for (int i = 0; i < commonIndices; ++i) {
-                    def numA = verA[i].toInteger()
-                    def numB = verB[i].toInteger()
-                    if (numA != numB) {
-                        return numA <=> numB
-                    }
-                }
-                verA.size() <=> verB.size() }
-            json << ["id":os + k, "url":baseUrl + latest.value];
-        }
-    }
-}
-
-lib.DataWriter.write("org.jenkins-ci.plugins.chromedriver.ChromeDriver",JSONObject.fromObject([list:json]));
+lib.DataWriter.write("org.jenkins-ci.plugins.chromedriver.ChromeDriver",JSONObject.fromObject([list:jsonList]))
