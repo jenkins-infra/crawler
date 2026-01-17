2e212f0c5b2cc0f76d2439cb92e98ad57d5dfc2d
Mark Waite <mark.earl.waite@gmail.com>
2026-01-17 06:09:31 -0700

    Fix allure commandline generator (#168)
    * Remove unused import from sbt.groovy
    
    * Fix allure commandline generator
    
    Fixes https://github.com/jenkins-infra/helpdesk/issues/4957
    
    Uses the same techniques that were used in the sbt tool downloader
    pull request:
    
    * https://github.com/jenkins-infra/crawler/pull/165
    
    Issue first detected in plugin BOM pull request:
    
    * https://github.com/jenkinsci/bom/pull/6225
    
    Fixes the Allure plugin portion of Jira issue:
    
    * https://issues.jenkins.io/browse/JENKINS-76355
    
    Fixes the Allure plugin portion of help desk ticket:
    
    * https://github.com/jenkins-infra/helpdesk/issues/4957
    
    Testing done:
    
    * Confirmed the list was empty before this change
    * Confirmed that this change provides available Allure commandline installers
    * Compared the last JSON file from the Wayback machine with the new file.
      Old file is missing recent commandline versions, but otherwise same as new file

--- allure.groovy
+++ allure.groovy
@@ -1,9 +1,10 @@
 #!./lib/runner.groovy
 // Generates server-side metadata for Allure
+import org.htmlunit.html.HtmlAnchor;
 import org.htmlunit.WebClient
-import org.htmlunit.html.DomElement
+import hudson.util.VersionNumber
 import org.htmlunit.html.HtmlPage
-import org.htmlunit.xml.XmlPage
+import java.util.regex.Pattern
 import net.sf.json.JSONObject
 
 def getList() {
@@ -11,38 +12,81 @@ def getList() {
     versions.addAll(getCentralVersions())
     versions.addAll(getSonatypeVersions())
     return versions
+            .findAll { it != null }
+            .sort { o1,o2 ->
+        try {
+            def v1 = new VersionNumber(o1.id)
+            try {
+                new VersionNumber(o2.id).compareTo(v1)
+            } catch (IllegalArgumentException _2) {
+                -1
+            }
+        } catch (IllegalArgumentException _1) {
+            try {
+                new VersionNumber(o2.id)
+                1
+            } catch (IllegalArgumentException _2) {
+                o2.id.compareTo(o1.id)
+            }
+        }
+    }
 }
+
 def getCentralVersions() {
-    String baseUrl = 'https://repo1.maven.org/maven2/io/qameta/allure/allure-commandline'
-    URL metaUrl = new URL("$baseUrl/maven-metadata.xml")
+    String baseUrl = 'https://repo1.maven.org/maven2/io/qameta/allure/allure-commandline/'
+    URL metaUrl = new URL(baseUrl)
 
     WebClient wc = new WebClient()
-    XmlPage meta = wc.getPage(metaUrl)
+    wc.setCssErrorHandler(new org.htmlunit.SilentCssErrorHandler());
+    wc.getOptions().setThrowExceptionOnScriptError(false);
+    wc.getOptions().setThrowExceptionOnFailingStatusCode(false);
+    HtmlPage p = wc.getPage(metaUrl);
 
-    List<String> versions = meta.getByXPath("//metadata/versioning/versions/version")
-            .collect() { DomElement e -> e.getTextContent() }
-            .findAll() { e -> !e.contains('BETA') }
-            .reverse()
+    def versions = []
+    def pattern = Pattern.compile("^([0-9]+.*)/\$");
+
+    p.getAnchors().each {
+        HtmlAnchor a ->
+        def m = pattern.matcher(a.hrefAttribute)
+        if (m.find()) {
+            def ver = m.group(1)
+            if (!ver.contains("BETA")) {
+                versions.add(ver)
+            }
+        }
+    }
 
     return versions.collect() { version ->
         return ["id"  : version,
                 "name": version,
-                "url" : String.format('%s/%s/allure-commandline-%s.zip', baseUrl, version, version)
+                "url" : String.format('%s%s/allure-commandline-%s.zip', baseUrl, version, version)
         ]
     }
 }
 
 def getSonatypeVersions() {
-    String baseUrl = 'https://oss.sonatype.org/content/repositories/releases/ru/yandex/qatools/allure/allure-commandline'
-    URL metaUrl = new URL("$baseUrl/maven-metadata.xml")
+    String baseUrl = 'https://oss.sonatype.org/content/repositories/releases/ru/yandex/qatools/allure/allure-commandline/'
+    URL metaUrl = new URL(baseUrl)
 
     WebClient wc = new WebClient()
-    XmlPage meta = wc.getPage(metaUrl)
+    wc.setCssErrorHandler(new org.htmlunit.SilentCssErrorHandler());
+    wc.getOptions().setThrowExceptionOnScriptError(false);
+    wc.getOptions().setThrowExceptionOnFailingStatusCode(false);
+    HtmlPage p = wc.getPage(metaUrl);
 
-    List<String> versions = meta.getByXPath("//metadata/versioning/versions/version")
-            .collect() { DomElement e -> e.getTextContent() }
-            .findAll() { e -> !e.contains('RC') }
-            .reverse()
+    def versions = []
+    def pattern = Pattern.compile("^([0-9]+.*)/\$");
+
+    p.getAnchors().each {
+        HtmlAnchor a ->
+        def m = pattern.matcher(a.hrefAttribute)
+        if (m.find()) {
+            def ver = m.group(1)
+            if (!ver.contains("RC")) {
+                versions.add(ver)
+            }
+        }
+    }
 
     return versions.collect() { version ->
         return ["id"  : version,
@@ -54,7 +98,7 @@ def getSonatypeVersions() {
 
 def getSonatypeArtifactUrl(String baseUrl, String version) {
     def artifactName = getSonatypeArtifactName(version);
-    return String.format('%s/%s/%s', baseUrl, version, artifactName);
+    return String.format('%s%s/%s', baseUrl, version, artifactName);
 }
 
 def getSonatypeArtifactName(String version) {
--- sbt.groovy
+++ sbt.groovy
@@ -2,7 +2,6 @@
 // Generates server-side metadata for sbt-launch auto-installation
 import org.htmlunit.html.*;
 import org.htmlunit.WebClient
-import org.htmlunit.xml.XmlPage
 import hudson.util.VersionNumber
 import java.net.HttpURLConnection
 import java.net.URL
